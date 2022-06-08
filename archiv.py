9aff10d6f1de64a25ead09daaef0ea1d2be3c9d7
Gizar <gizarir@gmail.com>
2022-06-08 12:15:19 +0300

    Add classes and Example polimorphizm

--- archiv.py
+++ archiv.py
@@ -2278,3 +2278,27 @@ else:
 finally:
     print('Выход из программы')
 
+# Пример полиморфизма
+class Player:
+    def ask(self):
+        pass
+
+    def move(self):
+        self.ask()
+
+class User(Player):
+    def ask(self):
+        print('Печать из User')
+
+class Ai(Player):
+    def ask(self):
+        print('Печать из Ai')
+
+a_ = User()
+
+b_ = Ai()
+
+a_.ask()
+b_.ask()
+
+
